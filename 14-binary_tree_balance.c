d72a008aaf45eaab7ab04bee56eb8eb6459ba57f
EricoMartin <ericomartin.0785@gmail.com>
2023-12-07 15:33:19 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -11,15 +11,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        int left, right;
+	int left, right;
 
-        if (tree == NULL)
-                return (0);
+	if (tree == NULL)
+		return (0);
 
-        left = binary_tree_height(tree->left);
-        right = binary_tree_height(tree->right);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
 
-        return (left - right);
+	return (left - right);
 }
 
 /**
