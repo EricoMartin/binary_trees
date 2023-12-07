c92ddbd51cb3bbfa2eec10ac156d914a5398e4fe
EricoMartin <ericomartin.0785@gmail.com>
2023-12-07 15:00:20 -0800

    14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -3,12 +3,12 @@
 #include "binary_trees.h"
 
 /**
-* binary_tree_height - function that measures the
-* height of a binary tree
-*
-* @tree: pointer to the root node of the tree
-* Return: height of tree
-*/
+ * binary_tree_height - function that measures the
+ * height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree
+ * Return: height of tree
+ */
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
